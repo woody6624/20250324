4d839f6a7bc38c795854b04396a71a93643a86ac
woody6624 <woody6624@gmail.com>
2025-04-09 09:49:01 +0900

    업데이트!

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,5 +1,7 @@
 package edu.fisa.ce;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 2 + 3;
+		System.out.println("하이");
+		assertEquals(5,result);
+		System.out.println("바이");
+
 	}
 
 }
